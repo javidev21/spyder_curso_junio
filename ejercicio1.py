6cdcc23013bac426e0d689bb873630d5772a9246
javidev21 <ingjaviersalas@gmail.com>
2025-06-08 14:33:39 -0600

    Ejercicio1

--- ejercicio1.py
+++ ejercicio1.py
@@ -6,3 +6,33 @@ Created on Sun Jun  8 13:37:12 2025
 @author: javier
 """
 
+import pandas as pd
+import numpy as np
+datos={'Nombre':['Leonardo','Darik','Alex','Alex'], 'Calificaciones':[
+    '60','70','70', '80'], 'Deportes':['Natación','Basquet', 'Fútbol', 'Fútbol'],
+    'Materias':['Química','Matemática', 'Historia', 'vida']
+       }
+df= pd.DataFrame(datos)
+print(df)
+print('\n'*4)
+print(df)
+print('\n')
+
+datos2={'Nombre':['Darik','Alex','Alex', 'N/A', 'Alex','Alex'], 
+        'Calificaciones':['60','70','100', np.nan, '200', '300'], 
+        'Deportes':['Natación','N/A', 'Fútbol', 'Fútbol','Fútbol', 'Fútbol'],
+        'Materias':['Química','Matemática', 'N/A', 'vida','N/A', 'vida']}
+df2= pd.DataFrame(datos2)
+print(df2)
+print('\n'*4)
+print(df2.info())
+print('\n'*4)
+print(df2.describe())
+print('\n'*4)
+nuevo = pd.DataFrame(df2)
+nuevo= nuevo.replace(np.nan,"0")
+
+nuevo['Calificaciones']= nuevo.Calificaciones.astype(int)
+print(nuevo.describe())
+print('\n'*4)
+print(nuevo.info())
